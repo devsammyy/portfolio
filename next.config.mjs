8b1da23ecb98f2878e48d40afcb965c5fb448ccd
devsammyy <yinkaadebayo31@gmail.com>
2024-10-15 23:47:06 +0100

    j

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    distDir: 'docs',
-    basePath: '/devsammyy.github.io',
-    assetPrefix:  '/devsammyy.github.io',
-};
+const nextConfig = {};
 
 export default nextConfig;
