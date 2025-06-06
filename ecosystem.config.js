3d35acf805166a4c9d717393fd22664aacf2446e
devsammyy <yinkaadebayo31@gmail.com>
2025-06-06 15:44:39 +0100

    ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: 'clevertechh',
+        script: 'npm',
+        args: 'start',
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
+  
